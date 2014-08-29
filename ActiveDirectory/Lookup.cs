d11beed6f43ee824a5f71a348c8713330815e91d
Ben <ben@cheetham.me.uk>
2014-08-29 09:30:46 +0100

    Initial code commit

---
+++ ActiveDirectory/Lookup.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Linq;
+using GuildfordBoroughCouncil.ActiveDirectory.Interfaces;
+
+namespace GuildfordBoroughCouncil.ActiveDirectory
+{
+    #region Concrete classes for JSON deserialisation
+
+    internal class User : IUser
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public string UserName { get; set; }
+        public string UserNameX { get; set; }
+
+        public string Department { get; set; }
+        public int DirectReports { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ManagerUserName { get; set; }
+        public string ManagerUserNameX { get; set; }
+        public string Office { get; set; }
+        public string Telephone { get; set; }
+        public string TelephoneXtn { get; set; }
+        public string Title { get; set; }
+    }
+
+    internal class Group : IGroup
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public string UserName { get; set; }
+        public string UserNameX { get; set; }
+
+        public string Description { get; set; }
+    }
+    #endregion
+
+    public class Lookup
+    {
+        private static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = Properties.Settings.Default.ActiveDirectoryServiceUri;
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        public static async Task<IEnumerable<IUser>> Councillors(string query = null)
+        {
+            using (var client = GetClient())
+            {
+                var response = client.GetAsync("Lookup/Councillors?term=" + query).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<IEnumerable<User>>();
+                }
+                else
+                {
+                    return new List<IUser>();
+                }
+            } 
+        }
+
+        public static async Task<IUser> User(string SamAccountName)
+        {
+            if (!String.IsNullOrWhiteSpace(SamAccountName))
+            {
+                using (var client = GetClient())
+                {
+                    var response = client.GetAsync("Lookup/Users?UserNames=" + SamAccountName).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var Users = await response.Content.ReadAsAsync<IEnumerable<User>>();
+                        return Users.FirstOrDefault() ?? new User();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static async Task<IEnumerable<IUser>> GroupMembers(string SamAccountName)
+        {
+            if (!String.IsNullOrWhiteSpace(SamAccountName))
+            {
+                using (var client = GetClient())
+                {
+                    var response = client.GetAsync("Lookup/GroupMembers/" + SamAccountName).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await response.Content.ReadAsAsync<IEnumerable<User>>();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static async Task<IEnumerable<string>> GroupSamAccountNamesForUser(string SamAccountName, bool Follow = true)
+        {
+            if (!String.IsNullOrWhiteSpace(SamAccountName))
+            {
+                using (var client = GetClient())
+                {
+                    var response = client.GetAsync("Lookup/UserGroups/" + SamAccountName + "?follow=" + Follow.ToString()).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await response.Content.ReadAsAsync<IEnumerable<string>>();
+                    }
+                }
+            }
+
+            return new List<string>();
+        }
+    }
+}
---
+++ Linq/IIdentity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using GuildfordBoroughCouncil.ActiveDirectory.Interfaces;
+
+namespace GuildfordBoroughCouncil.Linq
+{
+    public static class IIdentityExtensions
+    {
+        public static string NameX(this IIdentity identity)
+        {
+            return identity.Name.RemoveDownLevelDomain().ToLower();
+        }
+
+        public static IUser ActiveDirectory(this IIdentity identity)
+        {
+            return new Data.Models.User(identity.NameX()).ActiveDirectory;
+        }
+
+        public static string DisplayName(this IIdentity identity)
+        {
+            return identity.ActiveDirectory().Name;
+        }
+
+        public static Data.Models.User GetUser(this IIdentity identity)
+        {
+            return new Data.Models.User(identity.NameX());
+        }
+
+        public static Data.Models.AdObject GetAdObject(this IIdentity identity)
+        {
+            return new Data.Models.AdObject(identity.NameX());
+        }
+
+        public static IEnumerable<string> GetGroupSamAccountNames(this IIdentity identity)
+        {
+            return GuildfordBoroughCouncil.ActiveDirectory.Lookup.GroupSamAccountNamesForUser(identity.NameX()).Result;
+        }
+    }
+}
